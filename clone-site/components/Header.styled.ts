82e55cf4d82f4adbe05062daec53ca9a794e378a
taisei0123 <taisei8955@gmail.com>
2021-09-24 03:15:57 +0900

    create mobile header

--- clone-site/components/Header.styled.ts
+++ clone-site/components/Header.styled.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Box } from './Box.styled';
+import { Col } from './Col.styled';
 
 export const HeaderContainer = styled.div`
   display: block;
@@ -8,12 +9,152 @@ export const HeaderContainer = styled.div`
   background: rgba(0, 0, 0, 0.8);
 `;
 
+// Desktop
+
 export const DesktopHeaderMenuContainer = styled(Box)`
   height: 44px;
   width: 90%;
   justify-content: space-between;
   z-index: 1;
   margin: auto;
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
-export const DesktopHeaderIcon = styled.img``;
+export const DesktopHeaderIcon = styled.img`
+  cursor: pointer;
+`;
+
+// Mobile
+
+export const MobileBarIconContainer = styled(Box)`
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+export const MenuBarIcon = styled(Col)`
+  justify-content: space-between;
+  align-content: flex-start;
+  height: 8px;
+`;
+
+export const MenuBarIconLine = styled.div`
+  background: #fff;
+  height: 1px;
+  width: 17px;
+  cursor: pointer;
+`;
+
+export const MobileHeaderMenuContainer = styled(Box)`
+  height: 48px;
+  width: 100%;
+  justify-content: space-between;
+  z-index: 1;
+  padding: 0 16px;
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const MobileHeaderIcon = styled.img`
+  width: 17px;
+  cursor: pointer;
+`;
+
+// OpenMobileMenu
+
+export const OpenMobileMenuContainer = styled(Col)`
+  width: 100vw;
+  height: 100vh;
+  background: #1d1d1f;
+`;
+
+export const OpenMobileMenuHeaderContainer = styled(Col)``;
+
+export const OpenHeaderTopContainer = styled(Box)`
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 16px;
+`;
+
+export const CloseIcon = styled.div`
+  font-size: 20px;
+  color: #6e6e73;
+  cursor: pointer;
+`;
+
+export const InputContainer = styled(Box)`
+  justify-content: center;
+  width: 100%;
+  border-bottom: 1px solid #424245;
+`;
+
+export const InputIconContainer = styled(Box)`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const InputIcon = styled.img`
+  width: 30px;
+  height: 37px;
+  margin-left: 80px;
+  padding-left: 10px;
+  background: rgba(100, 100, 100, 0.13);
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+  @media (max-width: 450px) {
+    margin-left: 10px;
+  }
+`;
+
+export const Input = styled.input`
+  flex: 1;
+  height: 37px;
+  margin-bottom: 15px;
+  border: none;
+  background: rgba(100, 100, 100, 0.13);
+  color: #6e6e73;
+  font-size: 17px;
+  line-height: 1.23536;
+  font-weight: 400;
+  letter-spacing: -0.022em;
+  padding-left: 10px;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+`;
+
+export const InputSpace = styled.div`
+  height: 37px;
+  width: 80px;
+  @media (max-width: 450px) {
+    width: 10px;
+  }
+`;
+
+export const OpenMenuContainer = styled(Box)`
+  justify-content: center;
+  align-items: flex-start;
+  width: 100%;
+  background: #1d1d1f;
+`;
+
+export const OpenMenuWrapper = styled(Col)`
+  margin: 0 80px;
+  width: 100%;
+  @media (max-width: 450px) {
+    margin: 0 40px;
+  }
+`;
+
+export const OpenMenuListRow = styled(Box)`
+  justify-content: flex-start;
+  align-items: center;
+  border-bottom: 1px solid #424245;
+  max-height: 46px;
+`;
+
+export const OpenMenuListImage = styled.img`
+  height: 50px;
+`;
--- clone-site/components/Header.tsx
+++ clone-site/components/Header.tsx
@@ -2,10 +2,28 @@ import {
   HeaderContainer,
   DesktopHeaderMenuContainer,
   DesktopHeaderIcon,
+  MobileHeaderMenuContainer,
+  MobileBarIconContainer,
+  MenuBarIcon,
+  MenuBarIconLine,
+  MobileHeaderIcon,
+  OpenMobileMenuContainer,
+  OpenMobileMenuHeaderContainer,
+  OpenHeaderTopContainer,
+  CloseIcon,
+  InputContainer,
+  InputIcon,
+  Input,
+  InputSpace,
+  OpenMenuContainer,
+  OpenMenuWrapper,
+  OpenMenuListRow,
+  OpenMenuListImage,
 } from './Header.styled';
+import { useState } from 'react';
 
 const Header = () => {
-  const desktopImagesArray = [
+  const imagesArray = [
     '/apple-images/globalnav_apple_image__cxwwnrj0urau_large.svg',
     '/apple-images/globalnav_links_store_image__d8z9vxma66c2_large.svg',
     '/apple-images/globalnav_links_mac_image__fv4ktb435mum_large.svg',
@@ -18,13 +36,61 @@ const Header = () => {
     '/apple-images/globalnav_search_image__fca9mfoh8a2q_large.svg',
     '/apple-images/globalnav_bag_image__bmix8075eg4i_large.svg',
   ];
+
+  const [isToggled, setIsToggled] = useState(true);
+  const onChangeClick = () => setIsToggled(!isToggled);
+  const display = isToggled ? { display: 'none' } : { display: 'flex' };
+  const disappear = !isToggled ? { display: 'none' } : { display: 'flex' };
   return (
     <HeaderContainer>
+      {/* Desktop */}
       <DesktopHeaderMenuContainer>
-        {desktopImagesArray.map((image, i) => (
+        {imagesArray.map((image, i) => (
           <DesktopHeaderIcon src={image} key={i} />
         ))}
       </DesktopHeaderMenuContainer>
+      {/* Mobile */}
+      <MobileHeaderMenuContainer style={disappear}>
+        <MobileBarIconContainer>
+          <MenuBarIcon onClick={onChangeClick}>
+            <MenuBarIconLine />
+            <MenuBarIconLine />
+          </MenuBarIcon>
+        </MobileBarIconContainer>
+        <MobileHeaderIcon src={imagesArray[0]} />
+        <MobileHeaderIcon src={imagesArray[imagesArray.length - 1]} />
+      </MobileHeaderMenuContainer>
+      {/* After Opening */}
+      <OpenMobileMenuContainer style={display}>
+        <OpenMobileMenuHeaderContainer>
+          <OpenHeaderTopContainer>
+            <CloseIcon onClick={onChangeClick}>✕</CloseIcon>
+            <MobileHeaderIcon src={imagesArray[0]} />
+            <MobileHeaderIcon src={imagesArray[imagesArray.length - 1]} />
+          </OpenHeaderTopContainer>
+          <InputContainer>
+            <InputIcon src="/apple-images/globalnav_search_image__fca9mfoh8a2q_large_gray.svg" />
+            <Input placeholder="Search apple.com" />
+            <InputSpace />
+          </InputContainer>
+        </OpenMobileMenuHeaderContainer>
+        <OpenMenuContainer>
+          <OpenMenuWrapper>
+            {imagesArray.map((image, i) => {
+              if (
+                i != 0 &&
+                i != imagesArray.length - 2 &&
+                i != imagesArray.length - 1
+              )
+                return (
+                  <OpenMenuListRow key={i}>
+                    <OpenMenuListImage src={image} />
+                  </OpenMenuListRow>
+                );
+            })}
+          </OpenMenuWrapper>
+        </OpenMenuContainer>
+      </OpenMobileMenuContainer>
     </HeaderContainer>
   );
 };
